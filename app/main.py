5de412a618326be4c430c62a5d7c468058b939db
Stanislav Sudakov <stasiksudakov@gmail.com>
2025-02-07 16:18:55 +0200

    update main

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from celery.result import AsyncResult
 
 from app.schemas import CitiesList, TaskID, TaskStatus, Weather
@@ -13,8 +13,13 @@ async def send_list_of_city(cities: CitiesList):
     """
     Function that allows to send list of cities for processing.
     """
+    if not isinstance(cities.cities, list) or len(cities.cities) == 0:
+        raise HTTPException(
+            status_code=400,
+            detail="The cities parameter must be a non-empty list."
+        )
     task = get_weather.delay(cities.cities)
-    return {"task_id": task.id}
+    return TaskID(task_id=task.id)
 
 
 @app.get("/tasks/{task_id}")
@@ -26,11 +31,13 @@ async def get_task(task_id: str):
 
     if task_res.state == "SUCCESS":
         result = task_res.result
-        result_url = result.get("result_url", None)
+        result_urls = result.get("result_url", None)
+        if isinstance(result_urls, str):
+            result_urls = [result_urls]
         return TaskStatus(
             task_id=task_id,
             status=task_res.state,
-            result_url=result_url
+            result_urls=result_urls
         )
 
     elif task_res.state == "FAILURE":
@@ -41,10 +48,9 @@ async def get_task(task_id: str):
         )
 
     else:
-        return TaskStatus(
-            task_id=task_id,
-            status=task_res.state,
-            result_url=None
+        raise HTTPException(
+            status_code=404,
+            detail=f"Task with id: {task_id} was not found or in progress"
         )
 
 
