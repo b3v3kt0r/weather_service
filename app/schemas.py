b913b166e210a58eb6c6aced1bb2b262d5d6fd08
Stanislav Sudakov <stasiksudakov@gmail.com>
2025-02-09 01:06:04 +0200

    update schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,31 @@
+import re
 from typing import List, Optional
-from pydantic import BaseModel
+
+from pydantic import BaseModel, field_validator
+
+from app.logger import logger
 
 
 class CitiesList(BaseModel):
     """
-    Model for accepting list of cities.
+    Model for accepting a list of cities with validation.
     """
-    cities: List[str] = []
+    cities: List[str]
+
+    @field_validator("cities", mode="before")
+    @classmethod
+    def validate_cities(cls, value):
+        """
+        Method that validates city for allowed symbols.
+        Checks if a city consists of letters, spaces, and hyphens.
+        """
+        pattern = r"^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ\s\-']+$"
+        for city in value:
+            if not isinstance(city, str) or not re.match(pattern, city):
+                logger.error("Validation error: Invalid city name: %s", city)
+                raise ValueError(f"Invalid city name: {city}")
+
+        return value
 
 
 class TaskID(BaseModel):
@@ -37,12 +56,12 @@ class CityWeather(BaseModel):
         if isinstance(other, CityWeather):
             return self.city == other.city
         return False
-    
+
     def __hash__(self):
         return hash(self.city)
 
 
-class Weather(BaseModel):
+class RegionWeather(BaseModel):
     """
     Model for responding weather, regions, cities.
     """
