d0ac89489c753b724afa574ff0169bcb86da5499
Stanislav Sudakov <stasiksudakov@gmail.com>
2025-02-09 01:10:45 +0200

    fixes

--- app/celeryy.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-
-from app.weather.utils import get_weather
-
-
-celery_app = Celery("weather_service", broker="redis://redis:6379", backend="redis://redis:6379")
-
-celery_app.autodiscover_tasks(packages=["app.weather"])
